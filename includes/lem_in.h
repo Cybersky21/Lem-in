101fd8a0ec1bd4d70f1f37474df2d11d40afdd2f
Anastasia Crooks <acrooks@at-i1.21-school.ru>
2019-08-23 17:33:08 +0300

    update header

--- includes/lem_in.h
+++ includes/lem_in.h
@@ -38,12 +38,32 @@ typedef struct	s_map
 * tools
 */
 
-int		ft_mini_atoi(char *line);
-int		comments(char *str);
-void	ft_error(void);
-void	components(char **room, int f);
+int			main(int a, char **b);
+
+void 		ents(t_map *map, int fd);
+int			comments(char *str);
+int			ft_mini_atoi(char *line);
+void		ft_error(void);
+
+void		rooms(t_map *map, int fd);
+int			ft_start_and_end(t_map *map, int fd, char *str);
+void		ft_start(t_map *map, int fd, char *str);
+void		components(char **room, int f);
+t_room		*create_room(char **room);
+void		ft_end(t_map *map, int fd, char *str);
+void		over_room(t_map *map, char *str);
+void		rooms_in_array(t_map *map, int *f);
+// тут должна быть проверка на одинаковые комнаты by name
+void 		ft_sort_array(t_map *map);
+void 		create_links(t_map *map, char *str);
+t_room		*seach_room(t_room **rooms, unsigned i, unsigned j, char *name);
+
+int			bfs(t_map *map);
+t_list		*bfs_inc(t_list *startlinks);
+void		up_combination(t_list **new_combination, t_room *room);
+t_list		*step(t_list *combination);
 
 
-void	rooms_in_mass(t_map *map, int fd);
+void		rooms_in_mass(t_map *map, int fd);
 
 #endif
